fb06d65f6e7c32c19e99354f7e7315cec08cffff
Md. Muhitul Islam Shimanto <114284390+MuhitShimanto@users.noreply.github.com>
2026-06-16 20:47:16 +0600

    Add Query 2 to fetch user details by name

--- QUERY.sql
+++ QUERY.sql
@@ -81,3 +81,8 @@ INSERT INTO Bookings (booking_id, user_id, match_id, seat_number, payment_status
 SELECT match_id, fixture, base_ticket_price 
 FROM Matches 
 WHERE tournament_category = 'Champions League' AND match_status = 'Available';
+
+-- Query 2
+SELECT user_id, full_name, email 
+FROM Users 
+WHERE full_name ILIKE 'Tanvir%' OR full_name ILIKE '%Haque%';
